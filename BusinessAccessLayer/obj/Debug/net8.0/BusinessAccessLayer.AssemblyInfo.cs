f948c67dcb6f0a9eea751194a0a9b0811868759a
Jordi Balcells <jbalcellss@uoc.edu>
2024-04-13 22:33:23 +0200

    Revert "."
    This reverts commit 3e388385601ceb19dd6d987aaf0751df7918e71b.

--- BusinessAccessLayer/obj/Debug/net8.0/BusinessAccessLayer.AssemblyInfo.cs
+++ BusinessAccessLayer/obj/Debug/net8.0/BusinessAccessLayer.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BusinessAccessLayer")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d3c604e41e0d6c07d60095743cb15dfbcec3dcb4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+610aefcf865b122fea9d9ec94c18f78475b922c9")]
 [assembly: System.Reflection.AssemblyProductAttribute("BusinessAccessLayer")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BusinessAccessLayer")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
